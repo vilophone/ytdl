6dea5e6833dc97d04c65343008ca026834a09a2c
vilophone <64742558+vilophone@users.noreply.github.com>
2021-06-23 10:08:26 -0400

    Update ytdl.sh

--- ytdl.sh
+++ ytdl.sh
@@ -14,30 +14,9 @@ do
 	read x
 	if [[ $x == *"youtube"* ]]; then
 		youtube-dl --audio-format mp3 -x -i $x
-	elif [[ $x == *"album"* ]]; then
-   		spotdl -a $x
-		spotdl -o mp3 -l *.txt
-	elif [[ $x == *"playlist"* ]]; then
-   		spotdl -o mp3 -p $x
-		spotdl -o mp3 -l *.txt
-		echo "Would you like to keep this music in one playlist? (y/n)"
-		read b
-		if [[ $b == "y" ]]; then
-			echo "What would you like to name this playlist? "
-			read n
-			mv *.mp3* ~/Music
-			cd ~/Music
-			mkdir "$n"
-			mv *.mp3* "$n"
-		fi
-	elif [[ $x == *"track"* ]]; then
-   		spotdl -o mp3 -s $x
-	elif [[ $x == *"artist"* ]]; then
-  		spotdl -o mp3 -aa $x
-		spotdl -o mp3 -l *.txt
+	else 
+   		spotdl $x
 	fi
 	mv *.mp3* ~/Music
 	cd ~/Music/ ; ~/ytdl/./folderize.sh ; cd ~/ytdl/
 done
-
-# config always skip overwrite manually
